a67e70598153ae764e003f4853e86333b1dfb091
Hardelle Briones <hardelle.briones@cit.edu>
2023-05-24 19:17:12 +0800

    Save question embeddings to file for faster retrieval

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import pandas as pd
+import numpy as np
+import os
 from sentence_transformers import SentenceTransformer, util
 from bs4 import BeautifulSoup
-import urllib.request
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 from flask import Flask, request, jsonify
@@ -24,10 +25,7 @@ def preprocess_text(text, remove_tags=False):
     processed_text = " ".join(tokens)
     return processed_text
 
-
-url = 'https://drive.google.com/u/0/uc?id=1tyy8KC1JCCBEJ22VMxeYNso8LKjP7Ta3&export=download'
 csv_file_path = 'question_data.csv'
-urllib.request.urlretrieve(url, csv_file_path)
 
 question_data = pd.read_csv(csv_file_path)
 question_data = question_data.dropna()  # Drop rows with missing values
@@ -37,6 +35,18 @@ def encode_text(text):
     return model.encode([text])[0]
 
 question_embeddings = question_texts.apply(encode_text)
+
+# Check if precomputed embeddings exist
+embeddings_file_path = 'question_embeddings.npy'
+if os.path.isfile(embeddings_file_path):
+    question_embeddings = np.load(embeddings_file_path, allow_pickle=True)
+else:
+    question_embeddings = np.array([model.encode(question) for question in question_texts])
+    np.save(embeddings_file_path, question_embeddings)
+
+
+# Convert the question_embeddings to a DataFrame
+question_embeddings = pd.DataFrame(question_embeddings)
 
 app = Flask(__name__)
 CORS(app)
@@ -50,7 +60,10 @@ def search():
     user_input = data['query']
     user_input = preprocess_text(user_input)
     user_input_embedding = encode_text(user_input)
-    similarities = question_embeddings.apply(lambda x: util.cos_sim(user_input_embedding, x))
+    # Convert user_input_embedding to a DataFrame
+    user_input_embedding = pd.DataFrame([user_input_embedding])
+    similarities = question_embeddings.apply(lambda x: util.cos_sim(user_input_embedding.values[0], x.values), axis=1)
+
     k = 5
     top_k_indices = similarities.argsort()[::-1][:k]
     results = []
